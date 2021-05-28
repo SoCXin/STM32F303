35f067845a9efa6f01fb8f64cecd77d83e5c0da9
qitas <qitas@qitas.cn>
2021-05-28 13:25:14 +0800

    fix

---
+++ src/demo/freertos/src/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdint.h>
+#include "stm32f303xe.h"
+
+#include "FreeRTOSConfig.h"
+
+// FreeRTOS includes.
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+
+// Nucleo Board User LED and Button
+#define NUCLEO_LED_GPIO (GPIOA)
+#define NUCLEO_LED_PIN (5)
+
+#define NUCLEO_BUTTON_GPIO (GPIOC)
+#define NUCLEO_BUTTON_PIN (13)
+
+#endif
